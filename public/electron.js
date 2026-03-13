25c57a0c21b68cbcec291fab055026db90caf766
Mark Fairhurst <mark@fairhursts.net>
2026-03-13 15:50:29 +0000

    fix build

--- public/electron.js
+++ public/electron.js
@@ -48,7 +48,7 @@ function createWindow() {
   mainWindow.loadURL(
     isDev
       ? "http://localhost:3000"
-      : `file://${path.join(__dirname, "../build/index.html")}`,
+      : `file://${path.join(app.getAppPath(), "build/index.html")}`,
   );
 
   // Open the DevTools in development mode
